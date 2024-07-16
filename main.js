e25b1b828f8cc4a00025b328bb30be4562e8fd99
Cristyan Bernardo Fernandes <cristyanplsc@hotmail.com>
2024-07-16 11:59:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaTeclas.length; contador ++ ) {
+
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }    
+
+    tecla.onkeydown = function (evento) {
+        if( evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+        }
+      }
+      tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+      }
+}
